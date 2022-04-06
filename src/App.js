8053da4dc08af09580b3bcf396fe4a839781eea3
famzila <famzil.contact@gmail.com>
2022-04-06 15:52:36 +0200

    Add places to the map

--- src/App.js
+++ src/App.js
@@ -22,13 +22,13 @@ function App() {
 
   // dependency set to [coordinates, bounds], whenver they are updated relaunch the apie.
   useEffect(() => {
-    if(bounds){
+    if (bounds) {
       getPlacesData(bounds.sw, bounds.ne).then(data => {
-      console.log(data);
-      setPlaces(data);
-    })
+        console.log(data);
+        setPlaces(data);
+      })
     }
-    
+
   }, [coordinates, bounds])
   return (
     <>
@@ -37,10 +37,10 @@ function App() {
       <Grid container spacing={3} style={{ width: "100%" }} >
         {/* On mobile devices the item takes the whole place (12), on medium devices only (4) */}
         <Grid item ws={12} md={4}>
-          <List places={places}/>
+          <List places={places} />
         </Grid>
         <Grid item ws={12} md={8}>
-          <Map setCoordinates={setCoordinates} setBounds={setBounds} coordinates={coordinates} />
+          <Map setCoordinates={setCoordinates} setBounds={setBounds} coordinates={coordinates} places = {places} />
         </Grid>
       </Grid>
       <Footer />
--- src/components/Map/map.jsx
+++ src/components/Map/map.jsx
@@ -7,9 +7,12 @@ import Rating from "@material-ui/lab/Rating";
 // import mapStyles from '../../mapStyles';
 import useStyles from "./styles";
 
-const Map = ({ setCoordinates, setBounds, coordinates }) => {
+const Map = ({ setCoordinates, setBounds, coordinates, places }) => {
   const classes = useStyles;
-  const isMobile = useMediaQuery("(min-width: 600px)");
+  const isDesktop = useMediaQuery('(min-width:600px)');
+
+  const defaultImg =
+  "https://www.foodserviceandhospitality.com/wp-content/uploads/2016/09/Restaurant-Placeholder-001.jpg";
 
   return (
     <>
@@ -33,7 +36,24 @@ const Map = ({ setCoordinates, setBounds, coordinates }) => {
           onChildClick={() => {
             return null;
           }}
-        ></GoogleMapReact>
+        >
+            { places?.map((place, index) => (
+                <div key={index} className={ classes.makerContainer} lat={Number(place.latitude)} lng = {Number(place.longitude)}>
+                    {isDesktop 
+                      ? <LocationOnOutlinedIcon color="primary" fontSize="large" />
+                      : (
+                        <Paper elevation={3} className={classes.paper}>
+                            <Typography className={classes.typography} variant="subtitle2" gutterBottom> {place.name}</Typography>
+                            <img
+                              className={classes.pointer}
+                              src={place.photo ? place.photo.images.large.url : defaultImg}
+                            />
+                            <Rating name="read-only" size="small" value={Number(place.rating)} readOnly />
+                        </Paper>
+                      )}
+                </div>
+            ))}
+        </GoogleMapReact>
       </section>
     </>
   );
