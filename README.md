8784282a078ff16e8b25ee3be6391ab77dc41efc
famzila <famzil.contact@gmail.com>
2022-04-08 11:06:12 +0200

    Stable version of the app

--- README.md
+++ README.md
@@ -1,5 +1,10 @@
-# Trip adviser App with React, Material UI and Google Map
+# Trips App with React, Material UI and Google Map
 
+- For Map: Google Map API
+- For weather: Rapid Open Weather API
+- For Restaurants /hotel /attractions data: Rapid Travel Advisor API
+- For Map layer style: SNAZZY Maps
+- For UI: Material UI
 
 
 ## Available Scripts
@@ -14,17 +19,8 @@ Open [http://localhost:3000](http://localhost:3000) to view it in your browser.
 The page will reload when you make changes.\
 You may also see any lint errors in the console.
 
-### `npm test`
-
-Launches the test runner in the interactive watch mode.\
-See the section about [running tests](https://facebook.github.io/create-react-app/docs/running-tests) for more information.
-
 ### `npm run build`
 
 Builds the app for production to the `build` folder.\
 It correctly bundles React in production mode and optimizes the build for the best performance.
 
-The build is minified and the filenames include the hashes.\
-Your app is ready to be deployed!
-
-See the section about [deployment](https://facebook.github.io/create-react-app/docs/deployment) for more information.
--- src/App.js
+++ src/App.js
@@ -1,51 +1,103 @@
-import React, { useEffect, useState } from "react";
-// Normalizing the style with Material's CssBaseline
-import { CssBaseline, Grid } from "@material-ui/core";
+import React, { useState, useEffect } from 'react';
+import { CssBaseline, Grid } from '@material-ui/core';
 
-import Header from "./components/Header/header";
-import List from "./components/List/list";
-import Map from "./components/Map/map";
-import Place from "./components/Place/place";
-import Footer from "./components/Footer/footer";
-import { getPlacesData } from "./api";
+import { getPlacesData, getWeatherData } from './api/index';
+import Header from './components/Header/header';
+import List from './components/List/list';
+import Map from './components/Map/map';
 
-function App() {
-  const [places, setPlaces] = useState([]);
-  const [coordinates, setCoordinates] = useState({});
+const App = () => {
+  const [type, setType] = useState('restaurants');
+  const [rating, setRating] = useState('');
+
+  const [coords, setCoords] = useState({});
   const [bounds, setBounds] = useState(null);
 
-  useEffect(() => {
-    navigator.geolocation.getCurrentPosition(({ coords: { latitude, longitude } }) => {
-      setCoordinates({ lat: latitude, lng: longitude });
-    })
-  }, [])
+  const [weatherData, setWeatherData] = useState([]);
+  const [filteredPlaces, setFilteredPlaces] = useState([]);
+  const [places, setPlaces] = useState([]);
 
-  // dependency set to [coordinates, bounds], whenver they are updated relaunch the apie.
+  const [autocomplete, setAutocomplete] = useState(null);
+  const [childClicked, setChildClicked] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(() => {
+    let latitude = 48.856614;
+    let longitude = 2.3522219;
+    setCoords({ lat: latitude, lng: longitude });
+    
+    // Uncomment for geolocalization
+    // navigator.geolocation.getCurrentPosition(({ coords: { latitude, longitude } }) => {
+    //   setCoords({ lat: latitude, lng: longitude });
+    // });
+  }, []);
+
+  // Reexecute when rating has changed
+  useEffect(() => {
+    const filtered = places?.filter((place) => Number(place.rating) > rating);
+    if(filtered){
+      setFilteredPlaces(filtered);
+    }
+  }, [rating]);
+
+  // Reexecute when type or bounds have changed
   useEffect(() => {
     if (bounds) {
-      getPlacesData(bounds.sw, bounds.ne).then(data => {
-        console.log(data);
-        setPlaces(data);
-      })
+      setIsLoading(true);
+      if(coords){
+        getWeatherData(coords.lat, coords.lng)
+          .then((data) => setWeatherData(data));
+      }
+      if(type){
+        getPlacesData(type, bounds.sw, bounds.ne)
+          .then((data) => {
+            setPlaces(data?.filter((place) => place.name && place.num_reviews > 0));
+            setFilteredPlaces([]);
+            setRating('');
+            setIsLoading(false);
+          });
+      }
     }
+  }, [bounds, type]);
 
-  }, [coordinates, bounds])
+  const onLoad = (autoComplete) => setAutocomplete(autoComplete);
+
+  const onPlaceChanged = () => {
+    const lat = autocomplete.getPlace().geometry.location.lat();
+    const lng = autocomplete.getPlace().geometry.location.lng();
+
+    setCoords({ lat, lng });
+  };
+
   return (
     <>
       <CssBaseline />
-      <Header />
-      <Grid container spacing={3} style={{ width: "100%" }} >
-        {/* On mobile devices the item takes the whole place (12), on medium devices only (4) */}
-        <Grid item ws={12} md={4}>
-          <List places={places} />
+      <Header onPlaceChanged={onPlaceChanged} onLoad={onLoad} />
+      <Grid container spacing={3} style={{ width: '100%' }}>
+        <Grid item xs={12} md={9} style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+          <Map
+            setChildClicked={setChildClicked}
+            setBounds={setBounds}
+            setCoords={setCoords}
+            coords={coords}
+            places={filteredPlaces.length ? filteredPlaces : places}
+            weatherData={weatherData}
+          />
         </Grid>
-        <Grid item ws={12} md={8}>
-          <Map setCoordinates={setCoordinates} setBounds={setBounds} coordinates={coordinates} places = {places} />
+        <Grid item xs={12} md={3}>
+          <List
+            isLoading={isLoading}
+            childClicked={childClicked}
+            places={filteredPlaces.length ? filteredPlaces : places}
+            type={type}
+            setType={setType}
+            rating={rating}
+            setRating={setRating}
+          />
         </Grid>
       </Grid>
-      <Footer />
     </>
   );
-}
+};
 
 export default App;
