5ce223ec8c7e9e475cf8a6bf304792de2f80cca3
famzila <famzil.contact@gmail.com>
2022-04-09 07:21:49 +0200

    Simplify the project structure

---
+++ src/components/list.jsx
@@ -0,0 +1,57 @@
+import React, { useState, useEffect, createRef } from 'react';
+import { CircularProgress, Grid, Typography, InputLabel, MenuItem, FormControl, Select } from '@material-ui/core';
+
+import Place from './place';
+import useStyles from '../styles/list-styles.js';
+
+const List = ({ places, type, setType, rating, setRating, childClicked, isLoading }) => {
+  const [elRefs, setElRefs] = useState([]);
+  const classes = useStyles();
+
+  // the (_, i) means that we are not going to use the first param but the second only.
+  useEffect(() => {
+    setElRefs((refs) => Array(places?.length).fill().map((_, i) => refs[i] || createRef()));
+  }, [places]);
+
+  return (
+    <section>
+      <Typography variant="h4" className={classes.header}>What's around me?</Typography>
+      {isLoading ? (
+        <div className={classes.loading}>
+          <CircularProgress size="5rem" />
+        </div>
+      ) : (
+        <>
+        {/* Type list: restaurants, hotels or attractions */}
+          <FormControl className={classes.formControl}>
+            <InputLabel id="type">Type</InputLabel>
+            <Select id="type" value={type} onChange={(e) => setType(e.target.value)}>
+              <MenuItem value="restaurants">Restaurants</MenuItem>
+              <MenuItem value="hotels">Hotels</MenuItem>
+              <MenuItem value="attractions">Attractions</MenuItem>
+            </Select>
+          </FormControl>
+          {/* Place's rating */}
+          <FormControl className={classes.formControl}>
+            <InputLabel id="rating">Rating</InputLabel>
+            <Select id="rating" value={rating} onChange={(e) => setRating(e.target.value)}>
+              <MenuItem value="">All</MenuItem>
+              <MenuItem value="3">Above ★★★</MenuItem>
+              <MenuItem value="4">Above ★★★★</MenuItem>
+              <MenuItem value="4.5">Above ★★★★½</MenuItem>
+            </Select>
+          </FormControl>
+          <Grid container spacing={3} className={classes.list}>
+            {places?.map((place, i) => (
+              <Grid ref={elRefs[i]} key={i} item xs={12}>
+                <Place selected={Number(childClicked) === i} refProp={elRefs[i]} place={place} />
+              </Grid>
+            ))}
+          </Grid>
+        </>
+      )}
+    </section>
+  );
+};
+
+export default List;
